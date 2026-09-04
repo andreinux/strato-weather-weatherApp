7e47beb8f28bfbc05c447082c9f3d8f790e83472
unknown <andreiacojido@gmail.com>
2026-09-04 21:17:27 +0800

    user input

--- src/index.js
+++ src/index.js
@@ -1,25 +1,43 @@
 console.log("running...");
 import "./index.css"
-
 const apiKey = "5G228VLEJFUDAH365JTVPHJBW";
-let location = "Cebu City";
 
-async function getData(){
+
+
+const form = document.querySelector("#searchForm");
+const cityInput = document.querySelector("#cityInput");
+
+
+
+
+async function getData(location){
     try {
-        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&include=current&key=${apiKey}&contentType=json`);
+        console.log("Location:", location);
+
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(location)}?unitGroup=metric&include=current&key=${apiKey}&contentType=json`);
+
         if (!response.ok) {
-    throw new Error(`HTTP error: ${response.status}`);
-}
+            throw new Error(`HTTP error: ${response.status}`);
+        }
+
         const weatherData = await response.json();
-        console.log("Location: ",location);
         console.log("Temperature: ",weatherData.currentConditions.temp);
         console.log("Feels Like: ",weatherData.currentConditions.feelslike);
         console.log("Humidity: ",weatherData.currentConditions.humidity);
         console.log("Wind Speed: ",weatherData.currentConditions.windspeed);
         console.log("Conditions: ",weatherData.currentConditions.conditions);
-    }catch(error){
+    }
+    
+    catch(error){
        console.error(error);
     }
 }
 
-getData();
+
+form.addEventListener("submit", (e)=> {
+    e.preventDefault();
+    getData(cityInput.value);
+})
+
+
+
